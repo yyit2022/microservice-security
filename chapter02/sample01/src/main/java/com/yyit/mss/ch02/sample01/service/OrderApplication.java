0219750a6fb164421955e5357e6eb0a46b75e3a4
yanpingping <yanpp@nfky.com>
2022-04-28 09:30:07 +0800

    first commit

---
+++ chapter02/sample01/src/main/java/com/yyit/mss/ch02/sample01/service/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.yyit.mss.ch02.sample01.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String args[]) {
+
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
